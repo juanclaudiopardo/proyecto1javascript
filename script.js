3e343aea0a66a682184cb95e473de0555e33f97b
Juan Pardo <josefrecinosanitarios@yahoo.com.ar>
2022-03-07 21:27:05 -0300

    nuevo proyecto javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 34;
+document.querySelector('.guess').value = 13;
